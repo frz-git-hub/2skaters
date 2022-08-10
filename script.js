45f7c54c630fa6ca628a1ad5d3e6f033df14c6c9
frz-git-hub <fhajatdost@gmail.com>
2022-08-10 16:06:51 +0430

    video loading

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 
 function myFunction() {
+
   document.body.style.overflowY = "hidden";
-  setTimeout(showPage, 5000);
+  var req = new XMLHttpRequest();
+  req.open('GET', './asset/skate.mp4', true);
+
+  req.onload = function () {
+    // Onload is triggered even on 404
+    // so we need to check the status code
+    if (this.status === 200) {
+      showPage()
+      // var videoBlob = this.response;
+      // var vid = URL.createObjectURL(videoBlob); // IE10+
+      // Video is now downloaded
+      // and we can set it as source on the video element
+      // video.src = vid;
+    }
+  }
+  req.onerror = function () {
+    // Error
+    console.log("ERRRRRRORORORORO")
+  }
+
+  req.send();
+  
+  // setTimeout(showPage, 5000);
 }
 
 function showPage() {
